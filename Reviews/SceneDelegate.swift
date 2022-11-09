b68dd5a29cbfc19b79533d11e68a232279f05c87
João <joao@jdmoreira.me>
2022-11-09 22:28:52 +0100

    Handle delays and errors from the service / http and pass the business into ReviewsVC

--- Reviews/SceneDelegate.swift
+++ Reviews/SceneDelegate.swift
@@ -1,20 +1,49 @@
-//
-//  SceneDelegate.swift
-//  Reviews
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    let serviceProvider: ServiceProvider = HTTPServiceProvider()
+    // or MockedServiceProvider() for simulating errors, delays, etc..
 
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
 
-        window = UIWindow(windowScene: scene)
-        let viewController = ReviewsVC(nibName: nil, bundle: nil)
-        window?.rootViewController = viewController
+        // Simple VC with a spinner that we can use
+        // while waiting for the server response
+        let spinnerVC = UIViewController()
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.startAnimating()
+        spinnerVC.view.embed(spinner)
 
+        // Setup the window
+        window = UIWindow(windowScene: scene)
+        window?.backgroundColor = .white
+        window?.rootViewController = spinnerVC
         window?.makeKeyAndVisible()
+
+        serviceProvider.loadBusiness(with: .philsBurger) { [unowned self] result in
+            let vc = window?.rootViewController
+
+            switch result {
+            case .failure(let error):
+                vc?.presentError(title: "Error occurred",
+                                 message: "Unexpected error: \(error)")
+            case .success(let business):
+                guard let business = business else {
+                    vc?.presentError(title: "Error occurred",
+                                     message: "No business was found with for this id.")
+                    return
+                }
+                window?.rootViewController = ReviewsVC(with: business)
+            }
+        }
+    }
+}
+
+private extension UIViewController {
+    func presentError(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        present(alert, animated: true)
     }
 }
--- Reviews/ViewControllers/ReviewsVC.swift
+++ Reviews/ViewControllers/ReviewsVC.swift
@@ -5,6 +5,14 @@
 import UIKit
 
 class ReviewsVC: UIViewController {
+    private var business: Business!
+
+    private let userSectionContainer = UIView()
+
+    convenience init(with business: Business) {
+        self.init(nibName: nil, bundle: nil)
+        self.business = business
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
